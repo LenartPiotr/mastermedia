e7b8339d5354cab5bceab6011e3749b09b2d20e4
Emma Paris <48452012+piotr3pl@users.noreply.github.com>
2023-04-01 14:30:36 +0200

    create folder structure

---
+++ src/mastermedia/backend/FolderStructure.java
@@ -0,0 +1,17 @@
+package mastermedia.backend;
+
+import java.io.File;
+
+public class FolderStructure {
+    public static final String PUBLIC = "./Public";
+    public static final String SOURCE = "./Public/Source";
+    public static final String MINI = "./Public/Mini";
+    public static final String SORTED = "./Public/Sorted";
+
+    public static void createFolderStructure() {
+        new File(PUBLIC).mkdirs();
+        new File(SOURCE).mkdirs();
+        new File(MINI).mkdirs();
+        new File(SORTED).mkdirs();
+    }
+}
---
+++ src/mastermedia/frontend/Main.java
@@ -0,0 +1,10 @@
+package mastermedia.frontend;
+
+import mastermedia.backend.FolderStructure;
+
+public class Main {
+    public static void main(String[] args){
+        FolderStructure.createFolderStructure();
+        //
+    }
+}
