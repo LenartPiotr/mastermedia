1b27eaf1b898ecdce0f2bdd4e660a29b05ae6b11
Gloria Kuliś <gloria.kulis@student.pk.edu.pl>
2023-04-17 03:24:16 +0200

    add css to fxml files

--- src/main/java/mastermedia/frontend/controllers/HomePageController.java
+++ src/main/java/mastermedia/frontend/controllers/HomePageController.java
@@ -1,17 +1,38 @@
 package mastermedia.frontend.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import mastermedia.frontend.Main;
 
 import java.io.IOException;
 
 public class HomePageController {
 
+
+
+    @FXML
+    Button reloadButton;
+
     public Scene createScene() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home_page_view.fxml"));
-
         return new Scene(fxmlLoader.load());
+    }
+
+
+
+    @FXML
+    public void initialize() {
+
+
+
+        reloadButton.setOnAction(event -> {
+            // #TODO add action
+
+        });
+
+
     }
 
 
