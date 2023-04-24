d00db468ed4f05015d89ae07128ee0aedded99a3
Baranek05 <baranjakub0525@gmail.com>
2023-04-24 20:51:26 +0200

    connection with config file

--- src/main/java/mastermedia/backend/CreateRelocate.java
+++ src/main/java/mastermedia/backend/CreateRelocate.java
@@ -1,96 +1,82 @@
 package mastermedia.backend;
 
+
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CreateRelocate {
 
     private String inputPath;
     private String outputPath;
-    private static Map<Integer, String> month_map = Map
-        .ofEntries(Map.entry(1, "Styczeń"), Map.entry(2, "Luty"), Map.entry(3, "Marzec"), Map.entry(4, "Kwiecień"),
-                   Map.entry(5, "Maj"), Map.entry(6, "Czerwiec"), Map.entry(7, "Lipiec"), Map.entry(8, "Sierpień"),
-                   Map.entry(9, "Wrzesień"), Map.entry(10, "Październik"), Map.entry(11, "Listopad"),
-                   Map.entry(12, "Grudzień"));
+    private Map<String, String> fileTypeMap;
 
-    public CreateRelocate(String inputPath, String outputPath) {
+    private static Map<Integer, String> monthMap = Map.ofEntries(
+            Map.entry(1, "Styczeń"), Map.entry(2, "Luty"), Map.entry(3, "Marzec"), Map.entry(4, "Kwiecień"),
+            Map.entry(5, "Maj"), Map.entry(6, "Czerwiec"), Map.entry(7, "Lipiec"), Map.entry(8, "Sierpień"),
+            Map.entry(9, "Wrzesień"), Map.entry(10, "Październik"), Map.entry(11, "Listopad"), Map.entry(12, "Grudzień")
+    );
 
+    public CreateRelocate(String inputPath, String outputPath, String configFilePath) {
         this.inputPath = inputPath;
         this.outputPath = outputPath;
-
+        this.fileTypeMap = readConfigFile(configFilePath);
     }
 
     public void relocateFiles() {
-
         File inputDirectory = new File(inputPath);
-
-        if(!inputDirectory.isDirectory()) return;
-
-        // SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
-
-        for(File file : inputDirectory.listFiles()) {
-
-            try {
-
-                BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
-                // Date creationDate = new Date(attr.creationTime().toMillis());
-                LocalDate creationDate = LocalDate.from(attr.creationTime().toInstant());
-
-                // Calendar calendar = Calendar.getInstance();
-                // calendar.setTime(creationDate);
-                // int year = calendar.get(Calendar.YEAR);
-                // int month = calendar.get(Calendar.MONTH) + 1;
-
-                String folderType;
-
-                if(isImage(file.getName())) {
-
-                    folderType = "Zdjęcia";
-
-                }else if(isVideo(file.getName())) {
-
-                    folderType = "Filmy";
-
-                }else {
-
-                    continue;
-
-                }
-
-                File outputDirectory = new File(outputPath + "/" + month_map.get(creationDate.getMonthValue()) + " " +
-                                                creationDate.getYear() + " " + folderType);
-
-                if(!outputDirectory.exists()) { outputDirectory.mkdirs(); }
-
-                Files.move(file.toPath(), outputDirectory.toPath().resolve(file.getName()));
-
-            }catch(Exception e) {
-
-                e.printStackTrace();
-
-            }
-
+        if (!inputDirectory.isDirectory()) {
+            return;
         }
-
+        for (File file : inputDirectory.listFiles()) {
+            try {
+                BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+                LocalDate creationDate = LocalDate.from(attr.creationTime().toInstant());
+                String folderType = fileTypeMap.get(getFileExtension(file.getName()));
+                if (folderType == null) {
+                    continue;
+                }
+                File outputDirectory = new File(outputPath + "/" + monthMap.get(creationDate.getMonthValue()) + " " +
+                        creationDate.getYear() + " " + folderType);
+                if (!outputDirectory.exists()) {
+                    outputDirectory.mkdirs();
+                }
+                Files.move(file.toPath(), outputDirectory.toPath().resolve(file.getName()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    private static boolean isImage(String fileName) {
-
-        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-        return extension.equalsIgnoreCase("jpg") || extension.equalsIgnoreCase("jpeg") ||
-               extension.equalsIgnoreCase("png");
-
+    private Map<String, String> readConfigFile(String configFilePath) {
+        Map<String, String> map = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(configFilePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("=");
+                if (parts.length == 2) {
+                    map.put(parts[0], parts[1]);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
     }
 
-    private static boolean isVideo(String fileName) {
-
-        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-        return extension.equalsIgnoreCase("mp4") || extension.equalsIgnoreCase("avi") ||
-               extension.equalsIgnoreCase("mov");
-
+    private String getFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex == -1) {
+            return "";
+        }
+        return fileName.substring(lastDotIndex + 1);
     }
-
 }
+
+
